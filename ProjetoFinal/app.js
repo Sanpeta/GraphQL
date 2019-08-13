2ce93a8f438e58792052d97af87995f9471fc6f8
Sidnei de Souza Junior <sidneisonic@hotmail.com>
2019-08-13 15:05:54 -0300

    Adicionando alguns exemplos de query

--- ProjetoFinal/app.js
+++ ProjetoFinal/app.js
@@ -1,13 +1,49 @@
 const {ApolloServer, gql} = require('apollo-server')
 
-const typeDefs = gql`
+const typeDefs = gql`	
+	
+	type Usuario {
+		id: ID!
+		nome: String!
+		email: String!
+		idade: Int
+		salario: Float
+		vip: Boolean
+		teste: String
+	}
+
+	type Produto {
+		nome: String!
+		preco: Float!
+		desconto: Float
+		precoComDesconto: Float
+	}
+	
 	type Query {
 		ola: String
 		data: String
+		usuarioLogado: Usuario
+		produtoEmDestaque: Produto
+		numerosMegaSenha: [Int]
 	}
 `
 
 const resolvers = {
+	Usuario: {
+		salario(usuario) {
+			return usuario.salario
+		},
+		teste(usuario) {
+			return usuario.nome
+		}
+	},
+	Produto: {
+		precoComDesconto(produto) {
+			if (produto.desconto) {
+				return produto.preco * ( 1- produto.desconto)
+			}
+		}
+	},
 	Query: {
 		ola() {
 			return 'Baster Retorna uma String'
@@ -15,6 +51,29 @@ const resolvers = {
 		data() {
 			const data = new Date
 			return `${data}`
+		},
+		usuarioLogado() {
+			return {
+				id: 1,
+				nome: "Sidnei de Souza Junior",
+				email: "Sidnei@hotmail.com",
+				idade: 23,
+				salario: 1000000,
+				vip: true
+			}
+		},
+		produtoEmDestaque() {
+			return {
+				nome: "Notebook Gamer",
+				preco: 5000.00,
+				desconto: 0.15
+			}
+		},
+		numerosMegaSenha() {
+			//return [4, 8, 13, 27, 33, 52]
+			const crescente = (a, b) => a - b
+			return Array(6).fill(0).map(n => parseInt(Math.random() *60 + 1)).sort(crescente)
+
 		}
 	}
 }
