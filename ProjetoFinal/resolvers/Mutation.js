8f61b446f90e27281d79e3e7af97e0e312908abc
Sidnei de Souza Junior <sidneisonic@hotmail.com>
2019-08-14 13:47:31 -0300

    Adicionado o input dentro do Usuario.graphql e utilizando pelo Mutation.graphql e alterando o codigo na Mutation.js

--- ProjetoFinal/resolvers/Mutation.js
+++ ProjetoFinal/resolvers/Mutation.js
@@ -1,10 +1,22 @@
 const {usuarios, proximoId} = require('../data/db');
+function indiceUsuario(filtro) {
+    if (!filtro) {
+        return -1;
+    }
+    const {id, email} = filtro
+    if (id) {
+        return usuarios.findIndex(u => u.id === id);
+    } else if (email) {
+        return usuarios.findIndex(u => u.email === email);
+    }
+    return -1;
+}
 
 module.exports = {
     //{nome, idade, email}
-    novoUsuario(_, args) {
+    novoUsuario(_, {dados}) {
         //some vai procurar no array se exite o mesmo elemento no array
-        const emailExistente = usuarios.some(u => u.email === args.email);
+        const emailExistente = usuarios.some(u => u.email === dados.email);
 
         if (emailExistente) {
             throw new Error('Email Existente');
@@ -12,7 +24,7 @@ module.exports = {
 
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
         }
@@ -21,9 +33,9 @@ module.exports = {
         usuarios.push(novo)
         return novo
     },
-    excluirUsuario(_, {id}) {
+    excluirUsuario(_, {filtro}) {
         //findIndex, vai porcurar no array se tem o elemento igual em alguma parte do array e retornando o index
-        const i = usuarios.findIndex(u => u.id === id)
+        const i = indiceUsuario(filtro);
         if (i < 0) {
             return null
         }
